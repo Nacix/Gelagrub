9ade4a0fb100a50ce93f4cb087327afd194d8a07
Nacix <anorexicanalt@gmail.com>
2021-01-17 19:24:46 -0500

    Mass Refactor

--- lua/entities/lunasflightschool_gelagrub/cl_init.lua
+++ lua/entities/lunasflightschool_gelagrub/cl_init.lua
@@ -2,7 +2,7 @@ include("shared.lua")
 				-- local Pos = self:LocalToWorld( Vector(10,0,10) )
 				-- timer.Create( "tauntaunsound", 2, 0, function() sound.Play( "tauntaun/tauntaun"..math.random(1,9)..".wav", Pos, 75 ) end )
 function ENT:Think()
-	self:EmitSound("tauntaun/tauntaun"..math.random(1,9)..".wav")
+	self:EmitSound("tauntaun/tauntaun" .. math.random(1,9) .. ".wav")
 	self:SetNextClientThink( CurTime() + math.random(4,10) )
 	return true
 end
@@ -11,19 +11,15 @@ function ENT:DamageFX()
 	local HP = self:GetHP()
 	if HP == 0 then return end
 	if HP > self:GetMaxHP() * 0.5 then return end
-	self.nextDFX = self.nextDFX or 0	
+	self.nextDFX = self.nextDFX or 0
 	if self.nextDFX < CurTime() then
 		self.nextDFX = CurTime() + 0.05
-		if HP <= self:GetMaxHP() * 0.5 then
-			if math.random(0,45) < 3 then
-				if math.random(1,2) == 1 then
+		if HP <= self:GetMaxHP() * 0.5 and math.random(0,45) < 3 and math.random(1,2) == 1 then
 					local Pos = self:LocalToWorld( Vector(25,0,50) + VectorRand() * 3 )
 					local effectdata = EffectData()
-						effectdata:SetOrigin( Pos )
+					effectdata:SetOrigin( Pos )
 					util.Effect( "BloodImpact", effectdata, true, true )
 					sound.Play( "tauntaun/tauntaunpain.wav", Pos, 75 )
-				end
-			end
 		end
 	end
 end
@@ -32,7 +28,7 @@ function ENT:EngineActiveChanged( bActive )
 	-- if bActive then
 		-- self.ENG = CreateSound( self, "ambient/machines/train_idle.wav" )
 		-- self.ENG:PlayEx(0,0)
-		
+
 		-- self.DIST = CreateSound( self, "ambient/machines/train_idle.wav" )
 		-- self.DIST:PlayEx(0, 0)
 	-- else
@@ -56,7 +52,7 @@ function ENT:SoundStop()
 	if self.DIST then
 		self.DIST:Stop()
 	end
-	
+
 	if self.ENG then
 		self.ENG:Stop()
 	end
--- lua/entities/lunasflightschool_gelagrub/init.lua
+++ lua/entities/lunasflightschool_gelagrub/init.lua
@@ -10,7 +10,7 @@ function ENT:SpawnFunction( ply, tr, ClassName )
 	ent:SetPos( tr.HitPos + tr.HitNormal )
 	ent:Spawn()
 	ent:Activate()
-	
+
 	return ent
 end
 
@@ -25,36 +25,36 @@ end
 function ENT:OnTick()
 	local Pod = self:GetDriverSeat()
 	if not IsValid( Pod ) then return end
-	
+
 	local Driver = Pod:GetDriver()
-	
+
 	local FT = FrameTime()
-	
+
 	local PObj = self:GetPhysicsObject()
-	local MassCenterL = PObj:GetMassCenter()	
+	local MassCenterL = PObj:GetMassCenter()
 	local MassCenter = self:LocalToWorld( MassCenterL )
 	self:SetMassCenter( MassCenter )
-	
+
 	local Forward = self:GetForward()
 	local Right = self:GetRight()
 	local Up = self:GetUp()
-	
+
 	self:DoTrace()
-	
+
 	local Trace = self.GroundTrace
-	if self.WaterTrace.Fraction <= Trace.Fraction and !self.IgnoreWater and self:GetEngineActive() then
+	if self.WaterTrace.Fraction <= Trace.Fraction and not self.IgnoreWater and self:GetEngineActive() then
 		Trace = self.WaterTrace
 	end
-	
+
 	local EyeAngles = Angle(0,0,0)
 	local KeyForward = false
 	local KeyBack = false
 	local KeyLeft = false
 	local KeyRight = false
 	local Sprint = false
-	
+
 	if IsValid( Driver ) then
-		if Driver:LookupBone("ValveBiped.Bip01_R_Thigh")then
+		if Driver:LookupBone("ValveBiped.Bip01_R_Thigh") then
 			Driver:ManipulateBoneAngles(Driver:LookupBone("ValveBiped.Bip01_R_Thigh"), Angle(40,0,-20))
 			Driver:ManipulateBoneAngles(Driver:LookupBone("ValveBiped.Bip01_L_Thigh"), Angle(-40,0,20))
 		end
@@ -84,9 +84,9 @@ function ENT:OnTick()
 		end
 	end
 	local MoveSpeed = Sprint and self.BoostSpeed or self.MoveSpeed
-	
+
 	local IsOnGround
-	
+
 	if IsValid( Driver ) then
 		if self.jum and self.j > CurTime() then
 			IsOnGround = false
@@ -105,15 +105,15 @@ function ENT:OnTick()
 		IsOnGround = Trace.Hit and math.deg( math.acos( math.Clamp( Trace.HitNormal:Dot( Vector(0,0,1) ) ,-1,1) ) ) < 70
 		self.jum = false
 	end
-	
+
 	PObj:EnableGravity( not IsOnGround )
 	self.onG = IsOnGround
-	
+
 	if (IsOnGround) then
 		local pos = Vector( self:GetPos().x, self:GetPos().y, Trace.HitPos.z)
 		local speedVector = Vector(0,0,0)
-		
-		if IsValid( Driver ) && !Driver:lfsGetInput( "FREELOOK" ) && self:GetEngineActive() then
+
+		if IsValid( Driver ) and not Driver:lfsGetInput( "FREELOOK" ) and self:GetEngineActive() then
 			local lookAt = Vector(0,-1,0)
 			lookAt:Rotate(Angle(0,Pod:WorldToLocalAngles( EyeAngles ).y,0))
 			self.StoredForwardVector = lookAt
@@ -122,45 +122,45 @@ function ENT:OnTick()
 			lookAt:Rotate(Angle(0,self:GetAngles().y,0))
 			self.StoredForwardVector = lookAt
 		end
-		
+
 		local ang = self:LookRotation( self.StoredForwardVector, Trace.HitNormal ) - Angle(0,0,90)
 		if self:GetEngineActive() then
 			speedVector = Forward * ((KeyForward and MoveSpeed or 0) - (KeyBack and MoveSpeed or 0)) + Right * ((KeyLeft and MoveSpeed or 0) - (KeyRight and MoveSpeed or 0))
 		end
-		
+
 		self.deltaV = LerpVector( self.LerpMultiplier * FT, self.deltaV, speedVector )
 		self:SetDeltaV( self.deltaV )
 		pos = pos + self.deltaV
 		self:SetIsMoving(pos != self:GetPos())
-		
+
 		self.ShadowParams.pos = pos
 		self.ShadowParams.angle = ang
 		PObj:ComputeShadowControl( self.ShadowParams )
 	end
-	
+
 	local GunnerPod = self:GetGunnerSeat()
 	if IsValid( GunnerPod ) then
 		local Gunner = GunnerPod:GetDriver()
-		if Gunner ~= self:GetGunner() then
+		if Gunner != self:GetGunner() then
 			self:SetTurretDriver( Gunner )
 		end
 	end
-	
+
 	local TurretPod = self:GetTurretSeat()
 	if IsValid( TurretPod ) then
 		local TurretDriver = TurretPod:GetDriver()
-		if TurretDriver ~= self:GetTurretDriver() then
+		if TurretDriver != self:GetTurretDriver() then
 			self:SetTurretDriver( TurretDriver )
 		end
 	end
 	self:Gunner( self:GetGunner(), GunnerPod )
 	self:Turret( self:GetTurretDriver(), TurretPod )
-	
-	if self.LastSkin ~= self:GetSkin() then
+
+	if self.LastSkin != self:GetSkin() then
 		self.LastSkin = self:GetSkin()
 	end
-	
-	if self.LastColor ~= self:GetColor() then
+
+	if self.LastColor != self:GetColor() then
 		self.LastColor = self:GetColor()
 	end
 	--[[
@@ -178,7 +178,7 @@ function ENT:OnTick()
 		-- self:SetPlaybackRate(self:GetForwardVelocity()/333)
 	-- end
 	if not IsValid( Driver ) then return end
-	
+
 	if Driver:KeyDown( IN_SPEED ) and self:GetForwardVelocity() > 10 then
 		self.MoveSpeed = 200
 	else
